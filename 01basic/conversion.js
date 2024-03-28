93e339c09d374328efb44b3fd12b92f0e14cde85
sumit kumar <49075665+Sumitkumar01@users.noreply.github.com>
2024-03-28 14:04:02 +0000

    conversion data type

---
+++ 01basic/conversion.js
@@ -0,0 +1,23 @@
+let score = '90abc';
+// let score = undefined; // nan output
+// let score = null; // 0 output
+// true = 1 ; false = 0
+
+
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+// console.log(valueInNumber);//NaN not a number if value is '90abc'
+// console.log(typeof valueInNumber);
+
+
+let isLoggedIn = "a";
+
+let valueInBoolean = Boolean(isLoggedIn);
+
+console.log(valueInBoolean);
+
+// 1 = true; 0 = false; " "= false; "a" = true;
+
