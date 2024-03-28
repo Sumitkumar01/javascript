2a8ceead0aea85d5aa9166e61b67f73a7dae404a
sumit kumar <49075665+Sumitkumar01@users.noreply.github.com>
2024-03-28 13:51:08 +0000

    data type completed

---
+++ 01basic/02datatype.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+
+const number = 123;
+
+console.log(typeof number);//number
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+// alert("Hello World!"); // pop-up box with text
+// primitives data types
+// number = 123; 
+// string = "Hello";
+// boolean = true;
+// bigint = 123n;
+// null = null; // no value or standard value
+// undefined = undefined; 
+// symbol = Symbol("id"); // unique
+
+// non-primitive data types
+// object = {}; // empty object
+
