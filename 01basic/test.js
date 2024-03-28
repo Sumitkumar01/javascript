16b1a8e912fed703b961860e4e187dbba1ec8a8d
sumit kumar <49075665+Sumitkumar01@users.noreply.github.com>
2024-03-28 10:08:58 +0000

    complet

---
+++ 01basic/test.js
@@ -0,0 +1,10 @@
+console.log('Hello, world!');
+
+const accountId = 123456789012;
+let accountEmail ='test@example.com';
+var accountPassword = 'password123';
+accountCity = 'New York';
+
+let accountState
+
+console.table([accountEmail, accountPassword, accountCity, accountState])
