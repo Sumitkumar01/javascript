eb38109ddef4b1274401d331feaf4d8013ce4789
sumit <sk7627577@gmail.com>
2024-03-31 16:09:17 +0530

    part 2 complete

--- 02_basic/object_eg.js
+++ 02_basic/object_eg.js
@@ -18,7 +18,6 @@
 // console.log( person[sym]);
 // different way to create object
 
-
 // let person1 = new Person('John', 30, 'male', true);
 // console.log(person1.name); // Output: John
 // Object.freeze(person);
@@ -33,3 +32,57 @@
 
 // person.greet(); // Output: Hello!
 // console.log(person.greet());
+
+// *********************part 2*******************************
+
+//  constructor function
+
+// const user = new Object;
+
+const user = {};
+user.name = "John";
+user.age = 30;
+user.isMarried = true;
+
+//  constructor function
+
+//  nesting object
+
+// const user2 = {
+//   email: "john@gmail.com",
+//   fullName: {
+//     userfullName: {
+//       firstName: "John",
+//       lastName: "Doe",
+//     },
+//   },
+// };
+
+// console.log(user2.fullName.userfullName);
+
+//  object method
+
+// object concatination
+
+const user3 = {
+  name: "John",
+  age: 30,
+};
+
+const user4 = {
+  email: "john@gmail.com",
+  fullName: {
+    userfullName: {
+      firstName: "John",
+      lastName: "Doe",
+    },
+  },
+};
+
+const user5 = Object.assign({}, user3, user4);
+
+// console.log(user5);
+console.log(Object.keys(user5));// it will return all the keys of the object
+console.log(Object.values(user5));// it will return all the values of the object
+console.log(Object.entries(user5));// it will return all the key value pair of the object
+console.log(user5,hasOwnProperty("name"));// it will return true or false
