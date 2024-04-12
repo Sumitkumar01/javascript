28c68209edbdf1da4ae19874ca6dd7c735745421
sumit <sk7627577@gmail.com>
2024-04-12 22:31:51 +0530

    complete

---
+++ class-object/call.js
@@ -0,0 +1,20 @@
+function SetUserName(name) {
+    this.name = name;
+}
+
+function createUser(name, email, password) {
+    SetUserName.call(this, name)
+
+    this.email = email;
+    this.password = password;
+}
+
+const user1 = new createUser('John Doe', 'john@example.com', '123456');
+
+console.log(user1);
+
+
+
+
+
+https://xhamster.desi/videos/bestgonzo-erotic-oil-massage-lead-to-roughsex-8425385
